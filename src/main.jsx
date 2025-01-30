915b6d4f3aa4503e9b6384f1d01ba42a17008d6a
Zeeshan-leghari <zeeshangood3@gmail.com>
2025-01-30 20:01:27 +0500

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import CartPage from './page/CartPage';
 import Home from './page/Home';
 import PaymentPage from './page/PaymentPage';
 import Errorpage from './page/Errorpage';
@@ -16,8 +17,11 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home/>
       },
-    
-      
+      {
+        path: "cart",  
+        element: <CartPage />
+      }
+      ,
       {
         path:"cart/Payment",
         element:<PaymentPage/>
